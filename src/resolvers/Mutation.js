7aa1d4f59a2512ab459537c3112b1bdb75e0de5d
jasonxsiervo <43787504+jasonxsiervo@users.noreply.github.com>
2020-07-25 12:16:11 +0800

    First commit

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,91 @@
-const mutations = {};
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-module.exports = mutations;
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
+        const item = await ctx.db.mutation.createItem({
+            data: {
+            ...args
+            }
+        }, info);
+
+        return item;
+    },
+    updateItem(parent, args, ctx, info) {
+        // first, take a copy of the updates
+        const updates = { ...args };
+        // remove the ID from the updates
+        delete updates.id;
+        // run the update method
+        return ctx.db.mutation.updateItem(
+            {
+                data: updates,
+                where: {
+                    id: args.id,
+                },
+            }, info
+        );
+    },
+    async deleteItem(parent, args, ctx, info) {
+        const where = { id: args.id };
+        // 1. find the item
+        const item = await ctx.db.query.item({ where }, `{ id title }`);
+        // 2. check if they own that item, or have the permissions
+        // 3. delete
+        return ctx.db.mutation.deleteItem({ where }, info);
+    },
+    async signup(parent, args, ctx, info) {
+        // lowercase the email
+        args.email = args.email.toLowerCase();
+        // hash the password
+        const password = await bcrypt.hash(args.password, 10);
+        // create the user in the database
+        const user = await ctx.db.mutation.createUser(
+            {
+                data: {
+                    ...args,
+                    password,
+                    permissions: { set: ['USER']}
+                }
+            }, 
+        info
+        );
+
+        // create the jwt token for them
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        // we set the jwt as a cookie on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 24 * 365, // 1 year cookie
+        });
+        // we return the user to the browser
+        return user;
+    },
+    async signin(parent, { email, password }, ctx, info) {
+        // 1. check if their is user with that email
+        const user = await ctx.db.query.user({ where: {email }});
+        if(!user){
+            throw new Error(`NO such user found for email ${email}`);
+        }
+        // 2. Check if their password is correct
+        const valid = await bcrypt.compare(password, user.password);
+        if(!valid){
+            throw new Error(`Invalid password`);
+        }
+        // 3. generate the JWT token
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        // 4. set the cookie with the token
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365,
+        })
+        // 5. return the user
+        return user;
+    },
+    signout(parent, args, ctx, info) {
+        ctx.response.clearCookie('token');
+        return { message: 'Goodbye!' };
+    }
+};
+
+module.exports = Mutations;
